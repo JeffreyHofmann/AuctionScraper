0a8f1fe2636979fcae1e3ad5bdee1ea4826e5e26
Jeffrey Hofmann <jeffreyscotthofmann@gmail.com>
2019-03-01 14:51:40 -0500

    removed useless var

--- as.rb
+++ as.rb
@@ -8,8 +8,7 @@ pages = ARGV[1].to_i
 pages.times do |page_num|
   search_url = "https://www.ebay.com/sch/i.html?&_nkw="
   page = "&_pgn=#{page_num+1}"
-  url = search_url + search_term + page
-  response = RestClient.get url
+  response = RestClient.get search_url + search_term + page
   md = response.scan /results-listing\d+.*?href="(.*?)".*?(\$\d*.\.\d*)/
   md.each do |item_info|
     puts item_info[0]
