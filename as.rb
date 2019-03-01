774a0dec3d64dca546ef381b1301788d00b548eb
Jeffrey Hofmann <jeffreyscotthofmann@gmail.com>
2019-03-01 14:45:29 -0500

    ebay POC

---
+++ as.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'rest-client'
+
+search_term = ARGV[0]
+pages = ARGV[1].to_i
+
+pages.times do |page_num|
+  search_url = "https://www.ebay.com/sch/i.html?&_nkw="
+  page = "&_pgn=#{page_num+1}"
+  url = search_url + search_term + page
+  response = RestClient.get url
+  md = response.scan /results-listing\d+.*?href="(.*?)".*?(\$\d*.\.\d*)/
+  md.each do |item_info|
+    puts item_info[0]
+    puts item_info[1]
+  end
+end
